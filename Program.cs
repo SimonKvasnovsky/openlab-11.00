a1cbbbf4621e9dd449d9a3f69e2ba985ca38e460
Simon <simon.kvasnovsky@gmail.com>
2020-12-06 17:30:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace openlab_11._00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
